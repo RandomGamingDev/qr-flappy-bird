7279322e205d048c20208e34098ba97549558c64
RandomGamingDev <ethanshiyi1123@gmail.com>
2024-01-18 09:37:22 -0500

    closer

--- src/test.js
+++ src/test.js
@@ -1,16 +1,26 @@
+// Init
 let d = document;
 d.body.style = "margin:0;";
+
 let c = d.createElement("canvas");
-//c.width = 200;
-//c.height = 200;
-c.style = "width:100%;height:100%;display:block;";
 d.body.appendChild(c);
 var ctx = c.getContext("2d");
 
-ctx.rect(0, 0, c.width, c.height);
-ctx.fillStyle = "blue";
-ctx.fill();
+let resize = () => {
+    c.width = window.innerWidth;
+    c.height = window.innerHeight;
+};
+resize();
+window.addEventListener("resize", resize);
 
-ctx.beginPath();
-ctx.arc(95,50,40,0,2*Math.PI);
-ctx.stroke();
+let draw = () => {
+    ctx.rect(0, 0, c.width, c.height);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(95,50,40,0,2*Math.PI);
+    ctx.stroke();
+    window.requestAnimationFrame(draw);
+}
+draw();
